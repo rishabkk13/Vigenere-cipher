637c4e6dc024cbe0f01e38d889eba22b13d86a2c
Rishab K <37018364+rishabkk13@users.noreply.github.com>
2020-05-12 15:58:43 +0530

    Add files via upload

---
+++ matrix.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+ascii_min = ord(' ')
+ascii_max = ord('~')
+
+d = deque(chr(x) for x in range(ascii_min, ascii_max + 1))
+
+print(''.join(d))
+for i in range(len(d)-1):
+    d.rotate(-1)
+    print(''.join(d))
